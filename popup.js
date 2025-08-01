5fbec2868f43d151309dc237fba0c8a1c513ac1f
Jesse Hao <jehyu77@gmail.com>
2025-07-31 23:26:29 -0400

    copy picked color feature

--- popup.js
+++ popup.js
@@ -6,11 +6,14 @@ const eyedropper = new EyeDropper();
 const pickButton = document.querySelector('.pick-button');
 const background = document.querySelector('.color-background');
 const text = document.querySelector('.color-text');
+const copyButton = document.querySelector('.copy-button');
+const indicator = document.querySelector('.copied-indicator');
+let color;
 
 
 async function useEyedropper() { 
     try {
-        const color = await eyedropper.open();
+        color = await eyedropper.open();
         console.log(color.sRGBHex);
         displayColor(color.sRGBHex);
     } catch (err) {
@@ -28,8 +31,21 @@ function displayColor(hex) {
     text.innerHTML = `HEX Code: <br>${hex}`;
 }
 
+function copyColor() {
+    const copied = color ? color.sRGBHex : 'No color copied.';
+    navigator.clipboard.writeText(copied);
+    displayCopyText();
+}
+
+function displayCopyText() {
+    indicator.textContent = 'Copied to clipboard!'
+    indicator.classList.add('copied');
+}
+
 pickButton.addEventListener('click', () => {
     useEyedropper();
-}) 
+})
+
+copyButton.addEventListener('click', copyColor);
 
 }
